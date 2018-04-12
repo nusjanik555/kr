a4cf95ae12f3a90b0dd53b166996c893c6eee615
Sergey <sergey@oxygen.mk.ua>
2018-04-12 18:04:03 +0300

    ispravleno

--- js/script1.js
+++ js/script1.js
@@ -11,8 +11,8 @@
  }
  var rez = (a+b<4) ? 'мало' : 'много';
  alert(rez);
- 
-*/
+
+ */
 
 
 /*var a = +prompt('Введите первое число', '');
@@ -79,8 +79,8 @@
  default : rez = "ошибка"; break;
  }
  alert(rez);
- 
-/*
+
+ /*
  var zag = prompt('ежик колючий?', '');
  if (zag != null) {
  var n = confirm("Ви - уверены?");
@@ -100,105 +100,98 @@
  }
  }
 
-*/
- 
+ */
+
 /* var vozr = +prompt('Введите свой возраст от 20 до 69 лет', '');
-if (vozr < 20 || vozr > 69) {
-    alert("ввели не правильное число ");
-}
-else {
-    var des = Math.floor(vozr / 10);
-    var ed = (vozr % 10);
-    var f;
-    var e;
+ if (vozr < 20 || vozr > 69) {
+ alert("ввели не правильное число ");
+ }
+ else {
+ var des = Math.floor(vozr / 10);
+ var ed = (vozr % 10);
+ var f;
+ var e;
 
-    switch (des) {
-        case 2:
-            f = "двадцать";
-            break;
-        case 3:
-            f = "тридцать";
-            break;
-        case 4:
-            f = "сорок";
-            break;
-        case 5:
-            f = "пятдесят";
-            break;
-        case 6:
-            f = "шестьдесят";
-            break;
-    }
+ switch (des) {
+ case 2:
+ f = "двадцать";
+ break;
+ case 3:
+ f = "тридцать";
+ break;
+ case 4:
+ f = "сорок";
+ break;
+ case 5:
+ f = "пятдесят";
+ break;
+ case 6:
+ f = "шестьдесят";
+ break;
+ }
 
-    switch (ed) {
-        case 0:
-            e = "леть";
-            break;
-        case 1:
-            e = "один год";
-            break;
-        case 2:
-            e = "два года";
-            break;
-        case 3:
-            e = "три года";
-            break;
-        case 4:
-            e = "четыре года";
-            break;
-        case 5:
-            e = "пять лет";
-            break;
-        case 6:
-            e = "шесть лет";
-            break;
-        case 7:
-            e = "семь лет";
-            break;
-        case 8:
-            e = "восем лет";
-            break;
-        case 9:
-            e = "девять лет";
-            break;
-    }
-	
-    alert(f + " " + e);
+ switch (ed) {
+ case 0:
+ e = "леть";
+ break;
+ case 1:
+ e = "один год";
+ break;
+ case 2:
+ e = "два года";
+ break;
+ case 3:
+ e = "три года";
+ break;
+ case 4:
+ e = "четыре года";
+ break;
+ case 5:
+ e = "пять лет";
+ break;
+ case 6:
+ e = "шесть лет";
+ break;
+ case 7:
+ e = "семь лет";
+ break;
+ case 8:
+ e = "восем лет";
+ break;
+ case 9:
+ e = "девять лет";
+ break;
+ }
+
+ alert(f + " " + e);
+ }
+ */
+
+function f1() {
+    document.getElementById("op").style.width = "400px";
 }
-*/
-  
- function f1()
-{
-document.getElementById("op").style.width="400px";
-} 
-function f2()
-{
-document.getElementsByTagName("img")[1].style.position="absolute"; 
-document.getElementsByTagName("img")[1].style.left="200px";
-document.getElementsByTagName("img")[1].style.top="100px";
+function f2() {
+    document.getElementsByTagName("img")[1].style.position = "absolute";
+    document.getElementsByTagName("img")[1].style.left = "200px";
+    document.getElementsByTagName("img")[1].style.top = "100px";
 }
-function f3()
-{
-document.getElementsByTagName("img")[0].style.position="relative";
-document.getElementsByTagName("img")[0].style.left="250px";
-document.getElementsByTagName("img")[0].style.top="320px";
+function f3() {
+    document.getElementsByTagName("img")[0].style.position = "relative";
+    document.getElementsByTagName("img")[0].style.left = "250px";
+    document.getElementsByTagName("img")[0].style.top = "320px";
 }
-function f4()
-{
-document.getElementById("oppp").style.backgroundColor="yellow";
-} 
-function f5()
-{
-document.bgColor="red";
+function f4() {
+    document.getElementById("oppp").style.backgroundColor = "yellow";
 }
-function f6()
-{
-var a = +document.getElementById("sum").value;
-var d = +document.getElementById("sum2").value;
-var s = a + d;
-document.getElementById("sum3").value = s;
+function f5() {
+    document.bgColor = "red";
 }
-function f7()
-{
-document.getElementById("upp").style.fontSise="78pt";
+function f6() {
+    var a = +document.getElementById("sum").value;
+    var d = +document.getElementById("sum2").value;
+    var s = a + d;
+    document.getElementById("sum3").value = s;
+}
+function f7() {
+    document.getElementById("upp").style.fontSize = "78pt";
 } 
